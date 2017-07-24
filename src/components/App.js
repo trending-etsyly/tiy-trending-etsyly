2eeb9df66f9623a3a6a4c6ed746a3c593ddd0c20
zoe-c <find.zoec@gmail.com>
2017-07-24 14:44:28 -0400

    react app fs structure setup. first commit

---
+++ src/components/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import '../styles/App.css';
+
+// import components
+// import components
+import ItemCard from './ItemCard.js'
+import Footer from './Footer.js'
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/ItemCard.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+// import '../styles/ItemCard.css'
+
+export default class ItemCard extends Component {
+  render() {
+    return (
+      <div className="ItemCard">
+
+      </div>
+    );
+  }
+}
