2d0d712ffc27ca64bb8bb12192a9263838b51e6f
zoe-c <find.zoec@gmail.com>
2017-07-24 15:20:04 -0400

    add class component to Trending.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,14 +3,23 @@ import '../styles/App.css';
 
 // import components
 // import components
+import Trending from './Trending.js'
 import ItemCard from './ItemCard.js'
 import Footer from './Footer.js'
+
+// JSX REACT ELEMENTS. DROP IN BELOW WHEN NEEDED.
+// <ItemCard />
+
+
 
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+         <Trending />
+
+         <Footer />
 
       </div>
     );
---
+++ src/components/Trending.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+// import 'styles > make css if we need sep css file'
+
+export default class Trending extends Component {
+  render() {
+    return (
+      <div className="TrendingPage">
+      <div className="Banner"> place image and shop handmade quote here</div>
+      <h1><b> Popular items trending now</b></h1>
+      </div>
+    );
+  }
+}
