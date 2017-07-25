d8b43d162db3178e5429d608e9ef6c1d499297a0
zoe-c <find.zoec@gmail.com>
2017-07-25 13:14:57 -0400

    deleted banner placeholder.

--- src/components/Trending.js
+++ src/components/Trending.js
@@ -10,7 +10,7 @@ export default class Trending extends Component {
   render() {
     return (
       <div className="TrendingPage">
-      <div className="Banner"> place image and shop handmade quote here</div>
+      <div className="Banner"></div>
       <h1><b> Popular items trending now</b></h1>
       <br></br>
       <ItemCard />
