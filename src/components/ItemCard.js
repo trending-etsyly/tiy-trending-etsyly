4aa158a1f2b0586e3262c8f000a00167b08a4cb9
chadwick <cekeller6121@yahoo.com>
2017-07-25 13:00:48 -0400

    remove item description placeholder

--- src/components/ItemCard.js
+++ src/components/ItemCard.js
@@ -6,7 +6,6 @@ export default class ItemCard extends Component {
     return (
       <div className="ItemCard">
         <div className="cardImage">image placeholder</div>
-        <div className="cardDescription">description placeholder</div>
         <div className="titlePriceRow">
           <div className="cardTitle">title placeholder</div>
           <div className="cardPrice">price placeholder</div>
