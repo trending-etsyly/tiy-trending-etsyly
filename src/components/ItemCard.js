e3c4660976e05303f036180ab3f4e9075f661ac9
chadwick <cekeller6121@yahoo.com>
2017-07-26 14:23:38 -0400

    fetched pics render on app

--- src/components/ItemCard.js
+++ src/components/ItemCard.js
@@ -9,22 +9,25 @@ export default class ItemCard extends Component {
     this.state= {imageUrl: "", loginName: ""}
   }
   componentDidMount() {
-    console.log("props: ", this.props);
-    fetch(`https://openapi.etsy.com/v2/listings/${this.props.listingId}/images?fields=url_170x135&api_key=${apiKey}`)
+    // console.log("props: ", this.props);
+    fetch(`https://openapi.etsy.com/v2/listings/${this.props.listingId}/images?fields=url_570xN&api_key=${apiKey}`)
     .then(results => {
-      console.log("results: ", results);
+      // console.log("results: ", results);
       return results.json();
     }).then(data => {
-      console.log("data ", data);
+      // console.log("data ", data);
+      this.setState({imageUrl: data.results[0].url_570xN})
+      // console.log("this.state", this.state)
+      console.log("image url: ", this.state.imageUrl);
     })
   }
   render() {
     return (
       <div className="ItemCard">
-        <div className="cardImage">image placeholder</div>
+        <div className="cardImage"><img src={this.state.imageUrl} alt="product_image" /></div>
         <div className="titlePriceRow">
-          <div className="cardTitle">title placeholder</div>
-          <div className="cardPrice">price placeholder</div>
+          <div className="cardTitle">{this.props.title}</div>
+          <div className="cardPrice">{this.props.price}</div>
         </div>
       </div>
     );
