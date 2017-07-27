334a29ebb41bee7f0f3baada59a6b839942c785b
Brittany  Arsi <brittmacarsi@icloud.com>
2017-07-27 10:57:38 -0400

    some progress, one fetch

--- src/components/ItemCard.js
+++ src/components/ItemCard.js
@@ -1,30 +1,34 @@
 import React, { Component } from 'react';
 // import '../styles/ItemCard.css'
 
-const apiKey = "ja3evd3ncu2l2iji21q7m5qh";
+// const apiKey = "wta5sg99vllp9dr1b3xw5qc3";
 
 export default class ItemCard extends Component {
   constructor(props) {
     super(props);
-    this.state= {imageUrl: "", loginName: ""}
+    // this.state= {imageUrl: "", loginName: ""}
   }
-  componentDidMount() {
-    // console.log("props: ", this.props);
-    fetch(`https://openapi.etsy.com/v2/listings/${this.props.listingId}/images?fields=url_570xN&api_key=${apiKey}`)
-    .then(results => {
-      // console.log("results: ", results);
-      return results.json();
-    }).then(data => {
-      // console.log("data ", data);
-      this.setState({imageUrl: data.results[0].url_570xN})
-      // console.log("this.state", this.state)
-      console.log("image url: ", this.state.imageUrl);
-    })
-  }
+  // componentDidMount() {
+  //   // console.log("props: ", this.props);
+  //   fetch(`https://openapi.etsy.com/v2/listings/${this.props.listingId}/images?fields=url_570xN&api_key=${apiKey}`)
+  //   .then(results => {
+  //     // console.log("results: ", results);
+  //     return results.json();
+  //   }).then(data => {
+  //     // console.log("data ", data);
+  //     this.setState({imageUrl: data.results[0].url_570xN})
+  //     // console.log("this.state", this.state)
+  //     console.log("image url: ", this.state.imageUrl);
+  //   })
+  // }
+
   render() {
+    let imgUrl = `"background-image: url(${this.props.mainImage})"`;
+    console.log('ImageURL:', imgUrl);
     return (
       <div className="ItemCard">
-        <div className="cardImage"><img src={this.state.imageUrl} alt="product_image" /></div>
+        // <div className="cardImage"><img src={this.state.mainImage} alt="product_image" /></div>
+        <div className="cardImage" style={this.imgUrl}></div>
         <div className="titlePriceRow">
           <div className="cardTitle">{this.props.title}</div>
           <div className="cardPrice">{this.props.price}</div>
--- src/components/Trending.js
+++ src/components/Trending.js
@@ -6,7 +6,7 @@ import '../styles/font-awesome-4.7.0/css/font-awesome.css'
 import Footer from './Footer.js'
 import ItemCard from './ItemCard.js'
 
-const apiKey = "ja3evd3ncu2l2iji21q7m5qh";
+const apiKey = "wta5sg99vllp9dr1b3xw5qc3";
 
 export default class Trending extends Component {
    constructor(props) {
@@ -20,15 +20,16 @@ export default class Trending extends Component {
    }
 i
    componentDidMount() {
-      fetch(` https://openapi.etsy.com/v2/listings/trending?fields=listing_id,user_id,title,price,url&limit=${this.state.limit}&page=${this.state.page}&api_key=${apiKey}`)
+      fetch(`https://openapi.etsy.com/v2/listings/trending?includes=MainImage(url_570xN),User(login_name)&fields=listing_id,user_id,title,price,url&limit=${this.state.limit}&page=${this.state.page}&api_key=${apiKey}`)
       .then(results => {
             return results.json();
           })
       .then(data => {
+        console.log('data', data);
          let productList = data.results.map((product) => {
             return (
-            <ItemCard key={product.listing_id} listingId={product.listing_id} userId={product.user_id} title={product.title} price={product.price}
-            address={product.url}/> )
+            <ItemCard key={product.listing_id} listingId={product.listing_id} userId={product.user_id} loginName={product.User.login_name} title={product.title} mainImage={product.mainImage.url_570xN} price={product.price} address={product.url}/>
+          )
          })
             this.setState({
                products: productList,
@@ -37,7 +38,7 @@ i
             console.log("state", this.state.products);
           })
       .catch((error) => {
-          console.log("Error with Fetching : ", error);
+          // console.log("Error with Fetching : ", error);
        });
    }
 
