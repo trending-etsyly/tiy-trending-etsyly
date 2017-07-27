6f748cd98095967577f127fdb88e8c029c27216b
Brad Broge <brbroge@gmail.com>
2017-07-27 15:14:44 -0400

    adding buttons for page selector and page limit

--- src/components/ItemCard.js
+++ src/components/ItemCard.js
@@ -2,16 +2,16 @@ import React, { Component } from 'react';
 import '../styles/ItemCard.css'
 
 export default class ItemCard extends Component {
-  constructor(props) {
-    super(props);
-  }
+//   constructor(props) {
+//     super(props);
+//   }
 
   render() {
     return (
       <div className="ItemCard">
         <a href={this.props.address}>
           <div className="stuffff">
-            <img className="cardImage" src={this.props.mainImage}></img>
+            <img className="cardImage" alt="Products sold on Etsy" src={this.props.mainImage}></img>
           </div>
           <div className="titlePriceRow">
             <div className="cardTitle">{this.props.title}</div>
