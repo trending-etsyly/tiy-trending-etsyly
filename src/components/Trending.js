086c6ece55dad7fd38313deacf1b427f410e4b64
zoe-c <find.zoec@gmail.com>
2017-07-29 03:20:46 -0400

    style fixes, still needs more finessing. these quick fixes help a little with issues of text cut off in item title row. could be redone the right way. styled page navigation, added arrow +1/-1 pg navigation.. back arrow does not return to previously viewed data.. do we want to add a feature to make that work? it works other than that.

--- src/components/Trending.js
+++ src/components/Trending.js
@@ -15,6 +15,8 @@ export default class Trending extends Component {
       this.pagesComponent = this.pagesComponent.bind(this);
       this.updatePage = this.updatePage.bind(this);
       this.fetchItems = this.fetchItems.bind(this);
+      this.pageNavForward = this.pageNavForward.bind(this);
+      this.pageNavBack = this.pageNavBack.bind(this);
 
       this.state = {
          page: "1",
@@ -25,15 +27,23 @@ export default class Trending extends Component {
    }
 
    fetchItems = () => {
-     fetch(`https://openapi.etsy.com/v2/listings/trending?includes=MainImage(url_570xN),User(login_name)&fields=listing_id,user_id,title,price,url&limit=${this.state.limit}&page=${this.state.page}&api_key=${apiKey}`)
+     fetch(`https://openapi.etsy.com/v2/listings/trending?includes=MainImage(url_570xN),User(login_name),Shop(shop_name)&fields=listing_id,user_id,title,price,url&limit=${this.state.limit}&page=${this.state.page}&api_key=${apiKey}`)
      .then(results => {
            return results.json();
          })
      .then(data => {
         let productList = data.results.map((product) => {
+         if (product.User) {
            return (
              <ItemCard key={product.listing_id} listingId={product.listing_id} userId={product.user_id} loginName={product.User.login_name} title={product.title} price={product.price} address={product.url} mainImage={product.MainImage.url_570xN} />
            )
+         }
+         else {
+           return (
+             <ItemCard key={product.listing_id} listingId={product.listing_id} userId={product.user_id}  title={product.title} price={product.price} address={product.url}  />
+           )
+         }
+
        });
            this.setState({
               products: productList,
@@ -56,6 +66,37 @@ export default class Trending extends Component {
      });
      return this.fetchItems();
    }
+// ------------------------------------
+   pageNavForward = (event) => {
+     event.preventDefault()
+     var pgToNum = Number(this.state.page) + 1;
+     var newPg = String(pgToNum);
+     this.setState({
+       page: newPg,
+     });
+     return this.fetchItems();
+   }
+
+   pageNavBack = (event) => {
+     event.preventDefault()
+     var pgToNum = Number(this.state.page) - 1;
+     var newPg = String(pgToNum);
+     this.setState({
+       page: newPg,
+     });
+     return this.fetchItems();
+   }
+
+   pageNavArrows = () => {
+      return (
+         <div className="pageForwardBackward">
+         <button className="pageNavArrows" key="-1" onClick={this.pageNavForward} value="<"> ◀︎ </button>
+         <button className="pageNavArrows" key="+1" onClick={this.pageNavForward} value=">"> ▶︎ </button>
+         </div>
+      )
+   }
+// -----------------------------------
+
 
    updateLimit = (event) => {
      event.preventDefault();
@@ -69,12 +110,14 @@ export default class Trending extends Component {
    pagesComponent = () => {
     let numPages = Math.ceil(this.state.count/this.state.limit), pageButtons = []
 
-    for (let n = 1; n <= numPages; n++) {
+   //  NOTE: limited the number of page buttons rendering for aesthetic reasons
+   // NOTE: the REAL ETSY renders 8 pages total, always... changing the scope/ range of pg numbers that are viewable/availbe as you navigate through.... I have not found out how to do this just yet. this just limits the # of buttons available. we can change this.
+    for (let n = 1; n <= numPages && n <= 8; n++) {
       pageButtons.push(<button className="linkButton" key={n} onClick={this.updatePage} value={n}>{n}</button>)
     }
      return (
        <div className="pages">
-         {pageButtons}
+         {pageButtons}<div className="fillerEllipse"> ... </div>
        </div>
      )
    }
@@ -82,6 +125,7 @@ export default class Trending extends Component {
    limitComponent = () => {
      return (
        <div className="limit">
+          <div className="limitHeader"> Results Per Page: </div>
          <button className="linkButton" key="24" onClick={this.updateLimit} value="24">24</button>
          <button className="linkButton" key="48" onClick={this.updateLimit} value="48">48</button>
          <button className="linkButton" key="72" onClick={this.updateLimit} value="72">72</button>
@@ -96,14 +140,15 @@ export default class Trending extends Component {
          <div className="Banner">
             <h2> Shop unique and handmade items directly from creative people around the world </h2>
          </div>
-         <h1><b> Popular items trending now</b></h1>
+         <h1><b> Popular items for trending</b></h1>
          <br></br>
          <div className="productsDiv">
            {this.state.products}
          </div>
          <div className="pageSelector">
-           Page Selector: {this.pagesComponent()}
-           Page Limit: {this.limitComponent()}
+           {this.pagesComponent()}
+           {this.pageNavArrows()}
+            {this.limitComponent()}
          </div>
          <Footer />
       </div>
